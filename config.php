831aa7e234aa429855ccb51569ebbffd0f98b697
shrutipande <shrutipande31@gmail.com>
2017-05-29 06:43:08 -0700

    first commit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shreeganeshenterprises.co/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shreeganeshenterprises.co/');
+
+// DIR
+define('DIR_APPLICATION', '/home/shreeganu/public_html/catalog/');
+define('DIR_SYSTEM', '/home/shreeganu/public_html/system/');
+define('DIR_LANGUAGE', '/home/shreeganu/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/shreeganu/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/shreeganu/public_html/system/config/');
+define('DIR_IMAGE', '/home/shreeganu/public_html/image/');
+define('DIR_CACHE', '/home/shreeganu/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/shreeganu/public_html/system/download/');
+define('DIR_UPLOAD', '/home/shreeganu/public_html/system/upload/');
+define('DIR_MODIFICATION', '/home/shreeganu/public_html/system/modification/');
+define('DIR_LOGS', '/home/shreeganu/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'shree_ganesh');
+define('DB_PASSWORD', 'admin@123');
+define('DB_DATABASE', 'shree_ganesh');
+define('DB_PREFIX', 'oc_');
